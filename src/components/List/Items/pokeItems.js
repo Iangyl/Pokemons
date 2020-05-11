f193be83580f254c93bc537f54b5d911c9a134c3
Iangyl <vohanchib@gmail.com>
2020-05-12 00:18:55 +0300

    modernized styles and templates

--- src/components/List/Items/pokeItems.js
+++ src/components/List/Items/pokeItems.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './style.scss';
 
 class PokeItems extends React.Component{
     render(){
         return(
-            <li></li>
+            <li>
+                <div className='img-box'><img src={require('../../../assets/pictures/pokeball.png')} alt='pika4u.png' id='foot-logo' /></div>
+                <div className='name-box'>Name</div>
+            </li>
         )
     }
 }
