168618d99fd8306411ca6286a34071d6c4369286
Iangyl <vohanchib@gmail.com>
2020-05-11 23:52:56 +0300

    fixed templates

--- src/components/Article/informBlock.js
+++ src/components/Article/informBlock.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './style.scss';
 
 class InformBlock extends React.Component{
     render(){
         return(
             <div className='article'>
-
+                <div className='title-blc'>
+                    <h1>Title</h1>
+                </div>
+                <div className='img-blc'>
+                    <img src={require('../../assets/pictures/pokeball.png')} alt='pika4u.png' id='foot-logo' />
+                </div>
+                <div className='stats-blc'>
+                
+                </div>
             </div>
         )
     }
