ccf9f0f0accdb5a92f2cbe5dbfdf502eae6f73ef
Iangyl <vohanchib@gmail.com>
2020-05-14 17:57:42 +0300

    integrate initialization "props" in item class

--- src/components/List/Items/pokeItems.js
+++ src/components/List/Items/pokeItems.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './style.scss';
 
-//const { key, name } = this.props;
-
 class PokeItems extends React.Component{
     render(){
+        const {key, name} = this.props;
         return(
-            <li key={this.props.key}>
+            <li key={key}>
                 <div className='img-box'><img src={require('../../../assets/pictures/pokeball.png')} alt='pika4u.png' id='foot-logo' /></div>
-                <div className='name-box'>{this.props.name}</div>
+                <div className='name-box'>{name}</div>
             </li>
         )
     }
--- src/components/List/pokeList.js
+++ src/components/List/pokeList.js
@@ -6,12 +6,11 @@ import { connect } from 'react-redux';
 
 class PokeList extends React.Component{
     render(){
-        console.log(this.props.pokemonName);
         return(
             <div className='listBox'>
                 <ul id='list'>
                    {
-                        (typeof this.props.pokemonName == undefined) ? ( 
+                        (typeof this.props.pokemonName === undefined) ? ( 
                             this.props.pokemonName.map(item => {
                                 return (
                                     <PokeItems 
