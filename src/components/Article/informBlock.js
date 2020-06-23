340e8211166db465a1aab48ddbf4e2267ffb1240
Iangyl <vohanchib@gmail.com>
2020-06-23 19:51:40 +0300

    created componentDidMount func in file with detail info about pokemon and added local component state

--- src/components/Article/informBlock.js
+++ src/components/Article/informBlock.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
+
 import './style.scss';
 
 class InformBlock extends React.Component{
+    state = {
+        types: [],
+        description: '',
+        stats: {
+            hp: '',
+            attack: '',
+            defense: '',
+            speed: '',
+            specialAttack: '',
+            specialDefense: '',
+        },
+        height: '',
+        weight: '',
+        eggGroup: '',
+        abilities: '',
+        genderRatioMale: '',
+        genderRatioFemale: '',
+        evs: '',
+        hatchSteps: '',
+    };
+
+    async componentDidMount(){
+
+        //url`s for pokemons info
+
+        const pokemonUrl = this.props.pokeCreatureUrl;
+        const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${this.props.pokeCreatureIndex}/`;
+
+        //get poke info
+
+        const pokemonRes = await axios.get(pokemonUrl);
+        let { hp, attack, defense, speed, specialAttack, specialDefense } = '';
+    }
+
     render(){
-        console.log(this.props.pokeCreatureName);
         return(
             <div className='article'>
                 <div className='title-blc'>
@@ -25,6 +60,8 @@ export default connect(
     state => ({
         pokeCreatureName: state.pokemons.pokemonBlock.pokeName,
         pokeCreatureImg: state.pokemons.pokemonBlock.pokeImg,
+        pokeCreatureIndex: state.pokemons.pokemonBlock.pokeIndex,
+        pokeCreatureUrl: state.pokemons.pokemonBlock.pokeUrl,
     }),
     null,
 )(InformBlock);
